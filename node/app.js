5e5a701f9f459f92f486e7b7f28048376e3e1900
nicolascaliari <nicolascaliari28@gmail.com>
2023-08-28 14:05:21 -0300

    agregado de .env

--- node/app.js
+++ node/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
@@ -11,10 +12,12 @@ const { GoogleAuth } = require('google-auth-library');
 
 
 
+
+
 async function getValues(spreadsheetId, range) {
     try {
         const auth = new GoogleAuth({
-            keyFile: './src/json/credencial.json',
+            keyFile:process.env.GOOGLE_SHEETS_KEYFILE,
             scopes: 'https://www.googleapis.com/auth/spreadsheets',
         });
 
@@ -37,7 +40,7 @@ async function getValues(spreadsheetId, range) {
 
 
 
-const spreadsheetId = '1GWuEEJqcnOYwSVwfwhFh6N43OqANlplGaR5Ax-1NW20';
+const spreadsheetId = process.env.GOOGLE_SHEETS_SPREADSHEET_ID;
 const range = '!A1:E10';
 getValues(spreadsheetId, range)
     .then((result) => {
@@ -66,19 +69,19 @@ db.connect((err) => {
 
 
 async function insertarDatosEnGoogleSheets(status, items, direccion, telefono, payer) {
-    const spreadsheetId = '1GWuEEJqcnOYwSVwfwhFh6N43OqANlplGaR5Ax-1NW20'; // Reemplaza con el ID de tu hoja de cálculo
-    const range = '!A1:E10'; // Reemplaza con el nombre de la hoja en la que deseas insertar los datos
+    const spreadsheetId = process.env.GOOGLE_SHEETS_SPREADSHEET_ID;
+    const range = '!A1:E10'; 
 
-    // Configura la autenticación
+
     const auth = new GoogleAuth({
-        keyFile: './src/json/credencial.json', // Ruta a tu archivo de credenciales
+        keyFile: process.env.GOOGLE_SHEETS_KEYFILE, 
         scopes: 'https://www.googleapis.com/auth/spreadsheets',
     });
 
     const authClient = await auth.getClient();
     const sheets = google.sheets({ version: 'v4', auth: authClient });
 
-    // Formatea los datos como quieras insertarlos en la hoja de cálculo
+    
     const rowData = [payer.name, items.map(item => item.title).join(', '), direccion, telefono, status];
 
 
@@ -100,19 +103,18 @@ async function insertarDatosEnGoogleSheets(status, items, direccion, telefono, p
     }
 }
 
-// Ruta para recibir los datos de Mercado Pago
+
 app.post('/webhook', async (req, res) => {
     try {
-        // Aquí puedes procesar los datos recibidos de Mercado Pago
         const { status, items, payer } = req.body;
-        const direccion = payer.direccion; // Obtén la dirección de 'payer'
-        const telefono = payer.telefono; // Obtén el teléfono de 'payer'
+        const direccion = payer.direccion;
+        const telefono = payer.telefono; 
 
-        // Luego, llama a la función para insertar los datos en Google Sheets
+ 
         await insertarDatosEnGoogleSheets(status, items, direccion, telefono, payer);
 
 
-        // Responde con un mensaje de éxito
+
         res.status(200).json({ message: 'Datos guardados en Google Sheets con éxito' });
     } catch (error) {
         console.error('Error al procesar la solicitud:', error);
@@ -123,15 +125,15 @@ app.post('/webhook', async (req, res) => {
 
 
 
-// Rutas
+
 const productoRoutes = require('./src/routes/product');
 app.use('/producto', productoRoutes);
 
 const categoryRoutes = require('./src/routes/category');
 app.use('/category', categoryRoutes);
 
-const usuariosRoutes = require('./src/routes/user'); // Agrega las rutas de usuarios
-app.use('/usuarios', usuariosRoutes); // Usa '/usuarios' como prefijo para las rutas de usuarios
+const usuariosRoutes = require('./src/routes/user'); 
+app.use('/usuarios', usuariosRoutes); 
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
--- node/src/config/datebase.js
+++ node/src/config/datebase.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'agunic1004',
-    database: "barfer"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 
 module.exports = db;
